8a9ae12d5884ab0fc57c6e2c53b27b6c95dbb8ce
antikab <arsprosvet@gmail.com>
2025-03-23 16:58:37 +0300

    замена на стандартный fetch

--- src/composables/useBackgroundImage.js
+++ src/composables/useBackgroundImage.js
@@ -1,13 +1,12 @@
 import { ref } from 'vue'
-import { createClient } from 'pexels'
 // Константы
-import { IMAGE_API_KEY } from '@/constants'
+import { IMAGE_API_KEY, IMAGE_BASE_URL } from '@/constants'
 // Утилиты
 import { getBackgroundQuery } from '@/utils/utils'
 
 export function useBackgroundImage(weatherInfo) {
-  const backgroundImage = ref(new URL('@/assets/image/mrCat.jpg', import.meta.url).href)
-  const pexelsClient = createClient(IMAGE_API_KEY)
+  const defaultImage = new URL('@/assets/image/mrCat.jpg', import.meta.url).href
+  const backgroundImage = ref(defaultImage)
 
   // Запрос картинок через Pexels API
   async function updateBackgroundImage() {
@@ -17,20 +16,28 @@ export function useBackgroundImage(weatherInfo) {
     const temp = Math.round(weatherInfo.value?.main?.temp)
     const query = getBackgroundQuery(temp)
     const randomPage = Math.floor(Math.random() * 10) + 1
+    const url = `${IMAGE_BASE_URL}/search?query=${encodeURIComponent(query)}&page=${randomPage}&per_page=1`
     try {
-      const response = await pexelsClient.photos.search({
-        query,
-        page: randomPage,
-        per_page: 1
+      const response = await fetch(url, {
+        headers: {
+          Authorization: IMAGE_API_KEY
+        }
       })
-      if (response.photos && response.photos.length > 0) {
-        backgroundImage.value = response.photos[0].src.medium
+      if (!response.ok) {
+        throw new Error(`Ошибка запроса: ${response.status} ${response.statusText}`)
+      }
+
+      const data = await response.json()
+      console.log(data)
+
+      if (data.photos && data.photos.length > 0) {
+        backgroundImage.value = data.photos[0].src.medium
       } else {
-        backgroundImage.value = new URL('@/assets/image/mrCat.jpg', import.meta.url).href
+        backgroundImage.value = defaultImage
       }
     } catch (error) {
       console.error('Ошибка загрузки изображения с Pexels:', error.message)
-      backgroundImage.value = new URL('@/assets/image/mrCat.jpg', import.meta.url).href
+      backgroundImage.value = defaultImage
     }
   }
 
